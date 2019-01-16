074a5d1713855659db800ed1f827bfbe2b2311a0
candrawaskito <candradwiwaskito90@gmail.com>
2019-01-17 02:35:02 +0700

    added variable color and copyright

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -54,8 +54,8 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#2E8555',
-    secondaryColor: '#205C3B',
+    primaryColor: '#a587ff',
+    secondaryColor: '#49b4fa',
   },
 
   /* Custom fonts for website */
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Candra D Waskito for Sebo Studio`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
